5eb651f7aefcd3936a28e8ba85d6bf7006f98b68
BeedZiBood <117881296+BeedZiBood@users.noreply.github.com>
2024-02-09 20:23:41 +0300

    Delete shelkovnikov.dmitrii directory

--- shelkovnikov.dmitrii/S1/getQueueOfArithmeticExpression.h
+++
@@ -1,9 +0,0 @@
-#ifndef SPBSPU_LABS_2023_AADS_904_A_GETQUEUEOFARITHMETICEXPRESSION_H
-#define SPBSPU_LABS_2023_AADS_904_A_GETQUEUEOFARITHMETICEXPRESSION_H
-#include <queue.h>
-#include "partOfArithmeticExpression.h"
-namespace dimkashelk
-{
-  Queue< PartOfArithExpr > getQueueOfArithmeticExpression(std::string element);
-}
-#endif
--- shelkovnikov.dmitrii/S1/getResultArithmeticExpression.h
+++
@@ -1,9 +0,0 @@
-#ifndef SPBSPU_LABS_2023_AADS_904_A_GETRESULTARITHMETICEXPRESSION_H
-#define SPBSPU_LABS_2023_AADS_904_A_GETRESULTARITHMETICEXPRESSION_H
-#include <queue.h>
-#include "partOfArithmeticExpression.h"
-namespace dimkashelk
-{
-  long long getResultArithmeticExpression(Queue< PartOfArithExpr > &polandExpression);
-}
-#endif
