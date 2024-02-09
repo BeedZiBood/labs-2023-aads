c4a00b785911645f01237e9b224bc98c6c5fa5dc
BeedZiBood <117881296+BeedZiBood@users.noreply.github.com>
2024-02-09 20:24:08 +0300

    Delete tarasenko.ksenia directory

--- tarasenko.ksenia/S1/calculate.h
+++
@@ -1,7 +0,0 @@
-#ifndef CALCULATE_H
-#define CALCULATE_H
-namespace tarasenko
-{
-  long long calculate(long long a, long long b, char op);
-}
-#endif
--- tarasenko.ksenia/S1/calculateByPostfixQueue.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALCULATEBYPOSTFIXQUEUE_H
-#define CALCULATEBYPOSTFIXQUEUE_H
-#include "queue.h"
-#include "elem.h"
-namespace tarasenko
-{
-  long long calculateByPostfixQueue(Queue< Elem >& q_postfix);
-}
-#endif
