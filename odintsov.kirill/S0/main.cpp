633c882cd3362f2ef0519e3b4d2d52a2c75c510f
BeedZiBood <117881296+BeedZiBood@users.noreply.github.com>
2024-02-09 20:21:51 +0300

    Delete odintsov.kirill directory

--- odintsov.kirill/S0/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "outputCredentials.hpp"
-#include <iostream>
-
-int main()
-{
-  odintsov::outputCredentials(std::cout) << '\n';
-}
--- odintsov.kirill/S0/outputCredentials.cpp
+++
@@ -1,7 +0,0 @@
-#include "outputCredentials.hpp"
-#include <ostream>
-
-std::ostream& odintsov::outputCredentials(std::ostream& out)
-{
-  return out << "odintsov.kirill";
-}
